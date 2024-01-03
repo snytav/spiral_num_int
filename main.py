dac2efa29603d902b1a834c61abfbc72d2ff757e
Alexey Snytnikov <snytav@gmail.com>
2024-01-03 14:29:06 +0200

    example for odeint

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-# https://stackoverflow.com/questions/15928750/numerical-ode-solving-in-python
+# https://docs.sympy.org/latest/guides/solving/solve-ode.html#numerically-solve-an-ode-in-scipy
 
-
-import scipy.integrate as integrate
-import matplotlib.pyplot as plt
+from sympy import symbols, lambdify
 import numpy as np
-
-pi = np.pi
-sqrt = np.sqrt
-cos = np.cos
-sin = np.sin
-
-def deriv_z(z, phi):
-    u, udot = z
-    return [udot, -u + sqrt(u)]
-
-phi = np.linspace(0, 7.0*pi, 2000)
-zinit = [1.49907, 0]
-z = integrate.odeint(deriv_z, zinit, phi)
-u, udot = z.T
-# plt.plot(phi, u)
-fig, ax = plt.subplots()
-ax.plot(1/u*cos(phi), 1/u*sin(phi))
-ax.set_aspect('equal')
-plt.grid(True)
+import scipy.integrate
+import matplotlib.pyplot as plt
+# Create symbols y0, y1, and y2
+y = symbols('y:3')
+kf, kb = symbols('kf kb')
+rf = kf * y[0]**2 * y[1]
+rb = kb * y[2]**2
+# Derivative of the function y(t); values for the three chemical species
+# for input values y, kf, and kb
+ydot = [2*(rb - rf), rb - rf, 2*(rf - rb)]
+ydot
+t = symbols('t') # not used in this case
+# Convert the SymPy symbolic expression for ydot into a form that
+# SciPy can evaluate numerically, f
+f = lambdify((t, y, kf, kb), ydot)
+k_vals = np.array([0.42, 0.17]) # arbitrary in this case
+y0 = [1, 1, 0] # initial condition (initial values)
+t_eval = np.linspace(0, 10, 50) # evaluate integral from t = 0-10 for 50 points
+# Call SciPy's ODE initial value problem solver solve_ivp by passing it
+#   the function f,
+#   the interval of integration,
+#   the initial state, and
+#   the arguments to pass to the function f
+solution = scipy.integrate.solve_ivp(f, (0, 10), y0, t_eval=t_eval, args=k_vals)
+# Extract the y (concentration) values from SciPy solution result
+y = solution.y
+# Plot the result graphically using matplotlib
+plt.plot(t_eval, y.T) 
+# Add title, legend, and axis labels to the plot
+plt.title('Chemical Kinetics') 
+plt.legend(['NO', 'Br$_2$', 'NOBr'], shadow=True) 
+plt.xlabel('time') 
+plt.ylabel('concentration') 
+# Finally, display the annotated plot
 plt.show()
-
 qq = 0
