a2af5ab69427977b5c0f7508946159226ababe6f
Alexey Snytnikov <snytav@gmail.com>
2024-01-03 23:54:59 +0200

    cweight on spring

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ import numpy as np
 import scipy.integrate
 import matplotlib.pyplot as plt
 # Create symbols y0, y1, and y2
-y = symbols('y:3')
-kf, kb = symbols('kf kb')
-rf = kf * y[0]**2 * y[1]
-rb = kb * y[2]**2
+y = symbols('y:2')
+k, m = symbols('k m')
+# rf = kf * y[0]**2 * y[1]
+# rb = kb * y[2]**2
 # Derivative of the function y(t); values for the three chemical species
 # for input values y, kf, and kb
-ydot = [2*(rb - rf), rb - rf, 2*(rf - rb)]
+ydot = [y[1], -k/m*y[0]]
 ydot
 t = symbols('t') # not used in this case
 # Convert the SymPy symbolic expression for ydot into a form that
 # SciPy can evaluate numerically, f
-f = lambdify((t, y, kf, kb), ydot)
-k_vals = np.array([0.42, 0.17]) # arbitrary in this case
-y0 = [1, 1, 0] # initial condition (initial values)
+f = lambdify((t, y, k, m), ydot)
+k_vals = np.array([1.0, 1.0]) # arbitrary in this case
+y0 = [1,  0] # initial condition (initial values)
 t_eval = np.linspace(0, 10, 50) # evaluate integral from t = 0-10 for 50 points
 # Call SciPy's ODE initial value problem solver solve_ivp by passing it
 #   the function f,
@@ -33,10 +33,10 @@ y = solution.y
 # Plot the result graphically using matplotlib
 plt.plot(t_eval, y.T)
 # Add title, legend, and axis labels to the plot
-plt.title('Chemical Kinetics')
-plt.legend(['NO', 'Br$_2$', 'NOBr'], shadow=True)
+plt.title('Weight on a spring')
+plt.legend(['Position', 'Velocity'], shadow=True)
 plt.xlabel('time')
-plt.ylabel('concentration')
+plt.savefig('weight_spring.png')
 # Finally, display the annotated plot
 plt.show()
 qq = 0
