f154cd477b2541b986a7adee545ef955cb31b501
Alexey Snytnikov <snytav@gmail.com>
2024-01-03 12:36:36 +0200

    example for odeint

---
+++ main.py
@@ -0,0 +1,28 @@
+# https://stackoverflow.com/questions/15928750/numerical-ode-solving-in-python
+
+
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+import numpy as np
+
+pi = np.pi
+sqrt = np.sqrt
+cos = np.cos
+sin = np.sin
+
+def deriv_z(z, phi):
+    u, udot = z
+    return [udot, -u + sqrt(u)]
+
+phi = np.linspace(0, 7.0*pi, 2000)
+zinit = [1.49907, 0]
+z = integrate.odeint(deriv_z, zinit, phi)
+u, udot = z.T
+# plt.plot(phi, u)
+fig, ax = plt.subplots()
+ax.plot(1/u*cos(phi), 1/u*sin(phi))
+ax.set_aspect('equal')
+plt.grid(True)
+plt.show()
+
+qq = 0
